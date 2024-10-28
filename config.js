5fcb32049c3193c2fa367920292f4ad6f080d7ad
Sujayathulmeth <134757428+Sujayathulmeth@users.noreply.github.com>
2024-10-28 19:38:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=DyIiTJwY#Tgu3j8ybbnt9FH8R5mxpL5LEYPrjC3Zly4n91Z5kd8w",
+MONGODB: process.env.MONGODB || "mongodb+srv://sujaya:sujaya@cluster0.mcpcg.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
